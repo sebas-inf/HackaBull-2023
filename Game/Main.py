3d1081f81a8ccd65b586c3f94a9425fc86f17041
Yuinyan20 <6dinformaticasinfante@gmail.com>
2023-03-25 18:32:48 -0400

    game

--- Game/Main.py
+++ Game/Main.py
@@ -2,15 +2,17 @@ import pygame
 
 pygame.init() #Initializes pygames
 
+#Display section
 display_width = 800
-
 display_length = 600
-
 screen = pygame.display.set_mode((display_width, display_length)) #Display
-
 pygame.display.set_caption("Bullons") #Window Name
+background_image = pygame.image.load("Game\\art\\grass.png") #Map background
 
-background_image = pygame.image.load("grass.png") #Map backgroun
+#Colors
+pathColor = (204,171,120)
+pathOutlineColor = (190,145,103)
+
 
 on = True #Flag to run program
  
