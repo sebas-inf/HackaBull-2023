684fead798580b60d1106b95e1a8c782b9e0fb26
Yuinyan20 <6dinformaticasinfante@gmail.com>
2023-03-25 18:07:01 -0400

    commit

--- Game/Main.py
+++ Game/Main.py
@@ -1,1 +1,33 @@
 import pygame
+
+pygame.init() #Initializes pygames
+
+display_width = 800
+
+display_length = 600
+
+screen = pygame.display.set_mode((display_width, display_length)) #Display
+
+pygame.display.set_caption("Bullons") #Window Name
+
+background_color = (255, 255, 255) #White Background
+
+on = True
+ 
+while on:
+    if pygame.event == pygame.QUIT:
+        on = False
+
+    screen.fill(background_color)
+    
+    pygame.display.update()    
+    
+pygame.quit()
+
+
+
+
+
+
+
+
