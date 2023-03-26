a10b555663421c55e4fd2db303a6ec5b7f56a608
sneakybeakygit <cjmgames@gmail.com>
2023-03-26 11:45:52 -0400

    asdf

--- Game/Bulloon.py
+++ Game/Bulloon.py
@@ -2,20 +2,22 @@ import pygame
 from math import sqrt
 
 class Bulloon:
-    def __init__(self, health, x, y, vx, vy):
-        self.__health = health
-        self.__speed = health * 2
+    def __init__(self, level, x, y, vx, vy):
+        self.__health = level
+        self.__speed = level * 2
         self.__x = x
         self.__y = y
         self.__vx = vx
         self.__vy = vy
 
-        if health == 1: # set image as redbull
+        if self.__health == 1: # set image as redbull
             self.image = pygame.image.load("Game/art/redbull.png")
             self.__width, self.__height = self.image.get_size()
 
-        if health == 2:
-            pass
+        if self.__health == 2:
+            self.image = pygame.image.load("Game/art/bluebull.png")
+            self.__width, self.__height = self.image.get_size()
+            
 
         #self.__radius = (self.__width + self.__height) // 2 # set radius to be used in distance calculations
 
